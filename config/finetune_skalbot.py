591993fcfa561bc46a6c90bed8659008f809a427
skalthoff <antskalthoff@gmail.com>
2023-09-30 21:36:50 -0700

    saklbot added

--- config/finetune_skalbot.py
+++ config/finetune_skalbot.py
@@ -1,14 +1,14 @@
 import time
 
-out_dir = 'out-shakespeare'
+out_dir = 'out-skalbot2'
 eval_interval = 5
 eval_iters = 40
 wandb_log = False # feel free to turn on
-wandb_project = 'shakespeare'
+wandb_project = 'skalbot'
 wandb_run_name = 'ft-' + str(time.time())
 
-dataset = 'shakespeare'
-init_from = 'gpt2-xl' # this is the largest GPT-2 model
+dataset = 'skalbot'
+init_from = 'gpt2-medium' # this is the largest GPT-2 model
 
 # only save checkpoints if the validation loss improves
 always_save_checkpoint = False
@@ -17,8 +17,8 @@ always_save_checkpoint = False
 # 1 batch_size * 32 grad_accum * 1024 tokens = 32,768 tokens/iter
 # shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
 batch_size = 1
-gradient_accumulation_steps = 32
-max_iters = 20
+gradient_accumulation_steps = 128
+max_iters = 200
 
 # finetune at constant LR
 learning_rate = 3e-5
